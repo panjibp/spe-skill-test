687760aa34e2bfb9e4e41f46b73b0059068f9181
panjibp <panji.bp.bp@gmail.com>
2021-04-09 16:16:10 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+class SpeSkillTest
+{
+    public static function narcissisticNumber()
+    {
+        //
+    }
+
+    public static function parityOutlier()
+    {
+        //
+    }
+
+    public static function needleHaystack()
+    {
+        //
+    }
+
+    public static function blueOceanReverse()
+    {
+        //
+    }
+}

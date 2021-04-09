3b8a9f15a2cbd3a808aa15627b8dfb79d31129ff
panjibp <panji.bp.bp@gmail.com>
2021-04-09 16:41:33 +0700

    the blue ocean reverse

--- test.php
+++ test.php
@@ -17,12 +17,19 @@ class SpeSkillTest
         echo $haystack[1];
     }
 
-    public static function blueOceanReverse()
+    public static function blueOceanReverse($array, $single)
     {
-        //
+        $result = array_diff($array, $single);
+        var_dump($result);
     }
 }
 
 $spe = new SpeSkillTest;
 
+echo "Needle in the Haystack:<br>";
 $spe::needleHaystack(["red", "blue", "yellow", "black", "grey"], "blue");
+
+echo "<br><br>";
+
+echo "The Blue Ocean Reverse:<br>";
+$spe::blueOceanReverse([1, 2, 3, 4, 6, 10], [1]);

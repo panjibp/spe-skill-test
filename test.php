23c334d46e3de8958b86e83a86038df21b291bf1
panjibp <panji.bp.bp@gmail.com>
2021-04-09 16:53:25 +0700

    parity outlier

--- test.php
+++ test.php
@@ -7,9 +7,13 @@ class SpeSkillTest
         //
     }
 
-    public static function parityOutlier()
+    public static function parityOutlier($number)
     {
-        //
+        foreach ($number as $numb) {
+            if ($numb % 2 !== 0) {
+                echo "the only odd number is $numb";
+            }
+        };
     }
 
     public static function needleHaystack($haystack, $needle)
@@ -33,3 +37,8 @@ echo "<br><br>";
 
 echo "The Blue Ocean Reverse:<br>";
 $spe::blueOceanReverse([1, 2, 3, 4, 6, 10], [1]);
+
+echo "<br><br>";
+
+echo "Parity Outlier:<br>";
+$spe::parityOutlier([2, 4, 5, 6]);
